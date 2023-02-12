81cfc0bfca118c456fa60e39f5d88e45214e5317
Iku Rem <naterrie@student.42lyon.fr>
2023-02-12 02:40:49 +0100

    Correction on kiss command

--- Commandes/kiss.js
+++ Commandes/kiss.js
@@ -16,21 +16,14 @@ module.exports = {
     ],
     
     async run(bot, interaction, args) {
-        
-        try {
+    
 
         let user = interaction.options.getUser("membre")
        
-
         if(interaction.member.user.id == user.id) return interaction.reply({ content : "Tu ne peux pas t'embrasser toi même", ephemeral : true })
-        if(user.id == bot.user.id) return interaction.reply({ content : "Tu es un peu seul dans la vie, non ?", ephemeral : false })
+        if(user.id == bot.user.id) return interaction.reply({ content : "Désolé.e, tu ne peux pas embrasser des lignes de code :/", ephemeral : true })
 
-        await interaction.reply({ content : `<@${interaction.member.user.id}> à embrassé : <@${user.id}>`, ephemeral : false }) 
-
-        } catch (err) {
-
-            return interaction.reply({ content: "La commande marche pas tkt frr", ephemeral : true })
-        }
+        await interaction.reply({ content : `<@${interaction.member.user.id}> à embrassé.e : <@${user.id}>`, ephemeral : false }) 
 
     }
 }
