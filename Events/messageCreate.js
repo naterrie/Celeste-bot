97643d7033c49582caff6613e023947aa3641d6f
Nathan Terrier <naterrie@z2r12p4.42lyon.fr>
2023-04-18 18:32:27 +0200

    adding message folder for futur features

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,20 +1,10 @@
 const Discord = require("discord.js")
 
 module.exports = async (bot, message) => {
-
-    let prefix = "";
-
-    let messageArray = message.content.split(" ")
-    let commandName = messageArray[0].slice(prefix.length)
-    let args = messageArray.slice(1)
-
-    if(message.content === "feur" || message.content === "Feur" || message.content === "FEUR")
-    {
-        await message.reply('ouge')
-    }
-    // try {
-    // let command = require(`../Commandes/${commandName}`)
-    //     command.run(bot, message, args)
-    // } catch (err) {
-        // }
+    try {
+    let command = require(`../message/${message.content}`)
+        command.run(bot, message)
+    } catch (err) {
+        return
+        }
 }
